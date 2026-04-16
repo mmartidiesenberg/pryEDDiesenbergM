b642ef9d7a0d0f06ad743b2044745648bd27edc2
mmartidiesenberg <mmmartidiesenberg@gmail.com>
2026-04-16 11:10:37 -0300

    clsNodo

---
+++ pryEDDiesenbergM/clsNodo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryEDDiesenbergM
+{
+    internal class clsNodo
+    {
+        //Campos del Nodo
+        private Int32 cod;
+        private String nom;
+        private String tra;
+    }
+}
