4a77faa94f96fdcf7fa0f7098103bfb9adc2aecf
mmartidiesenberg <mmmartidiesenberg@gmail.com>
2026-04-16 11:56:21 -0300

    void agregar

---
+++ pryEDDiesenbergM/clsCola.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryEDDiesenbergM
+{
+    internal class clsCola
+    {
+        //campos de la clase
+        private clsNodo pri;
+        private clsNodo ult;
+
+        //propiedades de la clase
+        public clsNodo Primero
+        {
+            get { return pri; }
+            set { pri = value; }
+        }
+
+        public clsNodo Ultimo
+        {
+            get { return ult; }
+            set { ult = value; }
+        }
+        public void Agregar(clsNodo Nuevo)
+        {
+            if (pri == null)
+            {
+                pri = Nuevo;
+                ult = Nuevo;
+            }
+            else
+            {
+                ult.Siguiente = Nuevo;
+                ult = Nuevo;
+            }
+        }
+    }
+}
