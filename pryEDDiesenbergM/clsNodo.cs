d8324b61639187dcc21825b09e24be0895ce2131
mmartidiesenberg <mmmartidiesenberg@gmail.com>
2026-04-16 11:30:24 -0300

    .

--- pryEDDiesenbergM/clsNodo.cs
+++ pryEDDiesenbergM/clsNodo.cs
@@ -8,9 +8,34 @@ namespace pryEDDiesenbergM
 {
     internal class clsNodo
     {
-        //Campos del Nodo
+        //Campos del Nodo 
         private Int32 cod;
         private String nom;
         private String tra;
+        private clsNodo sig;
+
+        public Int32 Codigo
+        {
+            get { return cod; }
+            set { cod = value; }
+        }
+
+        public String Nombre
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+
+        public String Tramite
+        {
+            get { return tra; }
+            set { tra = value; }
+        }
+
+        public clsNodo Siguiente
+        {
+            get { return sig; }
+            set { sig = value; }
+        }
     }
 }
