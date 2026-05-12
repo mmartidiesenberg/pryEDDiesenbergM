1d109c7fd11fdfd885cd0a64134376472fe83cee
mmartidiesenberg <mmmartidiesenberg@gmail.com>
2026-05-12 11:40:04 -0300

    frmConsultaBD

--- pryEDDiesenbergM/frmConsultaBD.Designer.cs
+++ pryEDDiesenbergM/frmConsultaBD.Designer.cs
@@ -28,19 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtConsulta = new System.Windows.Forms.TextBox();
+            this.lblConsultaSQL = new System.Windows.Forms.Label();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.dgvConsultaBD = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsultaBD)).BeginInit();
             this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtConsulta
+            // 
+            this.txtConsulta.Location = new System.Drawing.Point(12, 32);
+            this.txtConsulta.Multiline = true;
+            this.txtConsulta.Name = "txtConsulta";
+            this.txtConsulta.Size = new System.Drawing.Size(776, 175);
+            this.txtConsulta.TabIndex = 2;
+            // 
+            // lblConsultaSQL
+            // 
+            this.lblConsultaSQL.AutoSize = true;
+            this.lblConsultaSQL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConsultaSQL.Location = new System.Drawing.Point(12, 9);
+            this.lblConsultaSQL.Name = "lblConsultaSQL";
+            this.lblConsultaSQL.Size = new System.Drawing.Size(150, 20);
+            this.lblConsultaSQL.TabIndex = 3;
+            this.lblConsultaSQL.Text = "Consulta en SQL:";
+            // 
+            // btnListar
+            // 
+            this.btnListar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListar.Location = new System.Drawing.Point(700, 213);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(88, 30);
+            this.btnListar.TabIndex = 4;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            // 
+            // dgvConsultaBD
+            // 
+            this.dgvConsultaBD.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvConsultaBD.Location = new System.Drawing.Point(12, 249);
+            this.dgvConsultaBD.Name = "dgvConsultaBD";
+            this.dgvConsultaBD.Size = new System.Drawing.Size(776, 241);
+            this.dgvConsultaBD.TabIndex = 5;
             // 
             // frmConsultaBD
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 502);
+            this.Controls.Add(this.dgvConsultaBD);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.lblConsultaSQL);
+            this.Controls.Add(this.txtConsulta);
             this.Name = "frmConsultaBD";
-            this.Text = "Consulta BD";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Consulta en la Base de Datos";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsultaBD)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtConsulta;
+        private System.Windows.Forms.Label lblConsultaSQL;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.DataGridView dgvConsultaBD;
     }
 }
